66e4bbc6898e9fa1a3a766a2ef9ee789269a4386
teorver <paul.britko@gmail.com>
2024-03-03 20:07:28 +0100

    feat: define utils and base styles

---
+++ src/utils/types.ts
@@ -0,0 +1,15 @@
+export interface IBook {
+    title: string;
+    subtitle: string;
+    isbn13?: string;
+    price: string;
+    image: string;
+    url?: string;
+    rating: string;
+    desc?: string;
+    authors?: string;
+    publisher?: string;
+    pages?: string;
+    year?: string;
+    pdf?: string[];
+}
