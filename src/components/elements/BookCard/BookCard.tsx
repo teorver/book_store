0695b61c878dc7d65fa2cf71ac57f20591d05001
teorver <paul.britko@gmail.com>
2024-03-07 00:47:45 +0100

    feat(Book card): add component for a single book card

---
+++ src/components/elements/BookCard/BookCard.tsx
@@ -0,0 +1,22 @@
+import { IBook } from '../../../utils/types';
+import './BookCard.css';
+import { getRandomRating } from '../../../utils/helpers';
+
+const BookCard = ({ image, title, subtitle, price }: IBook) => {
+
+    return (
+        <div className="card-wrapper">
+            <img src={image} alt="#" />
+            <div className="bookCard">
+                <p className="book-title">{title}</p>
+                <span className="book-subtitle">{subtitle}</span>
+                <div className="book-footer">
+                    <span>{price}</span>
+                    <span>{getRandomRating()}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
