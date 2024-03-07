598b295b126f40d5a6bae91545d3a92080f4780f
teorver <paul.britko@gmail.com>
2024-03-07 23:44:23 +0100

    feat: implement BookCard v2.0

--- src/components/elements/BookCard/BookCard.tsx
+++ src/components/elements/BookCard/BookCard.tsx
@@ -6,10 +6,12 @@ const BookCard = ({ image, title, subtitle, price }: IBook) => {
 
     return (
         <div className="card-wrapper">
-            <img src={image} alt="#" />
+            <div className="img-wrapper">
+                <img src={image} alt="#" className="book-img"/>
+            </div>
             <div className="bookCard">
-                <p className="book-title">{title}</p>
-                <span className="book-subtitle">{subtitle}</span>
+            <p className="book-title">{title}</p>
+            <span className="book-subtitle">{subtitle}</span>
                 <div className="book-footer">
                     <span>{price}</span>
                     <span>{getRandomRating()}</span>
