2667b9af287d37871dff1c5fe577a2a4428e7c80
teorver <paul.britko@gmail.com>
2024-03-08 18:32:19 +0100

    feat: implement App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Header from "./components/elements/Header/Header.tsx";
+import { Route, Routes } from 'react-router-dom';
+
+import Header from './components/elements/Header/Header';
+import Home from './components/pages/Home/Home';
+import Footer from './components/elements/Footer/Footer';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Routes>
+          <Route path="/"
+                 element={<Home />}
+          />
+      </Routes>
+      <Footer />
     </div>
   );
 }
