ee603a69347b323c9c269787600e44a7b89f4dff
teorver <paul.britko@gmail.com>
2024-03-21 17:15:33 +0100

    fix: remove unused code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,7 +15,6 @@ const Home = () => {
     const { books } = useSelector((state: RootState) => state.fullBookList);
 
     const fetchBooks = async () => {
-        dispatch(fetchBooksRequest());
         const booksInfo = await getBooksInfo();
         dispatch(fetchBooksSuccess(booksInfo));
     }
